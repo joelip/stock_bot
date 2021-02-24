156c650a3699110df8d959751e9db85b2048be82
joelip <lipperjj@gmail.com>
2021-02-24 13:38:31 -0800

    Get bot working with NBA top shot
    - Refine selectors for NBA topshot.
    - Generalize Slack module for sending messages

--- main.js
+++ main.js
@@ -5,22 +5,26 @@ const SendSlackMessage = require('./src/slack');
 var hasSentMessage = false;
 
 const run = async function () {
-  const TOP_SHOT_PACK_LENGTH = 8;
+  const TOP_SHOT_PACK_LENGTH = 2;
   const TOP_SHOT_URL = 'https://www.nbatopshot.com/packs';
-  const TOP_SHOT_PACKS_SELECTOR = "[alt='Pack Sold Out']";
+  const SELECTORS = {
+    statusLabel: "[class^='StatusLabel__StyledStatusLabel']",
+  };
   const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(TOP_SHOT_URL);
-  await page.waitForSelector(TOP_SHOT_PACKS_SELECTOR);
-  const $packImageElms = await page.$$(TOP_SHOT_PACKS_SELECTOR);
-  const numberOfPackSoldOutImages = $packImageElms.length;
-  console.log(`Found ${numberOfPackSoldOutImages} images`);
+  await page.waitForSelector(SELECTORS.statusLabel);
+  const currentDropStatus = await page.$eval(SELECTORS.statusLabel, (node) => {
+    return node.innerText;
+  });
+  console.log(currentDropStatus);
   if (
     numberOfPackSoldOutImages !== TOP_SHOT_PACK_LENGTH &&
     hasSentMessage === false
   ) {
     await SendSlackMessage(
-      `The number of "sold out" packs is no longer at max. Check ${TOP_SHOT_URL}.`
+      `The number of "sold out" packs has changed. Check ${TOP_SHOT_URL}.`,
+      '#nba-top-shot'
     );
     hasSentMessage = true;
   }
--- src/slack.js
+++ src/slack.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const { WebClient } = require('@slack/web-api');
 
-module.exports = async function (message = 'Testing') {
+module.exports = async function (
+  message = 'Please configure a message',
+  channel = '#general'
+) {
   const token = process.env.SLACK_TOKEN;
   const web = new WebClient(token);
   // See: https://api.slack.com/methods/chat.postMessage
   const res = await web.chat.postMessage({
-    channel: '#nba-top-shot',
+    channel: channel,
     text: message,
   });
 
