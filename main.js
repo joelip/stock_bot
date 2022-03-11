1827c4500c0ad4a9eecdefcb526d570d7c95832b
Joe Lipper <lipperjj@gmail.com>
2022-03-11 00:32:14 -0800

    Update main.js with PS5 digital

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const cron = require('node-cron');
+const BestBuyPS5DigitalFn = require('./src/bestbuyps5digital.js')
 const BestBuyPS5DiscFn = require('./src/bestbuyps5disc.js')
 
 const run = async function () {
   BestBuyPS5DiscFn();
+  BestBuyPS5DigitalFn();
 };
 
 cron.schedule('*/10 * * * * *', run);
