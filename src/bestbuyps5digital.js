71c8a95dd5caa8ba15ff0ad97a476c245d32f8d8
joelip <lipperjj@gmail.com>
2022-03-12 14:20:08 -0800

    Add error handling for timeouts

--- src/bestbuyps5digital.js
+++ src/bestbuyps5digital.js
@@ -4,36 +4,43 @@ const BestBuyPS5DigitalConfig = require('./site_configs').bestbuyps5digital;
 const iPhone = devices['iPhone 11 Pro'];
 
 module.exports = async function () {
-  const browser = await chromium.launch({ headless: true });
-  const context = await browser.newContext({
-    ...iPhone,
-    javaScriptEnabled: false
-  });
-  const page = await context.newPage();
-  await page.goto(BestBuyPS5DigitalConfig.url);
-  await page.waitForSelector(BestBuyPS5DigitalConfig.selectors.stockAvailabilityContainer);
-  const currentAvailabilityStatus = await page.$eval(
-    BestBuyPS5DigitalConfig.selectors.stockAvailabilityContainer,
-    (node) => {
-      return node.innerText;
-    }
-  );
-  console.log(
-    `${
-      BestBuyPS5DigitalConfig.itemName
-    } status as of ${new Date()} —— ${currentAvailabilityStatus}`
-  );
-  if (
-    BestBuyPS5DigitalConfig.hasSentMessage === false &&
-    currentAvailabilityStatus !== BestBuyPS5DigitalConfig.outOfStockString
-  ) {
-    console.log('Would have sent a message');
-    await SendSlackMessage(
-      BestBuyPS5DigitalConfig.changedMessage(),
-      BestBuyPS5DigitalConfig.channelName
+  try {
+    const browser = await chromium.launch({ headless: true });
+    const context = await browser.newContext({
+      ...iPhone,
+      javaScriptEnabled: false,
+    });
+    const page = await context.newPage();
+    await page.goto(BestBuyPS5DigitalConfig.url);
+    await page.waitForSelector(
+      BestBuyPS5DigitalConfig.selectors.stockAvailabilityContainer
     );
-    BestBuyPS5DigitalConfig.hasSentMessage = true;
+    const currentAvailabilityStatus = await page.$eval(
+      BestBuyPS5DigitalConfig.selectors.stockAvailabilityContainer,
+      (node) => {
+        return node.innerText;
+      }
+    );
+    console.log(
+      `${
+        BestBuyPS5DigitalConfig.itemName
+      } status as of ${new Date()} —— ${currentAvailabilityStatus}`
+    );
+    if (
+      BestBuyPS5DigitalConfig.hasSentMessage === false &&
+      currentAvailabilityStatus !== BestBuyPS5DigitalConfig.outOfStockString
+    ) {
+      console.log('Would have sent a message');
+      await SendSlackMessage(
+        BestBuyPS5DigitalConfig.changedMessage(),
+        BestBuyPS5DigitalConfig.channelName
+      );
+      BestBuyPS5DigitalConfig.hasSentMessage = true;
+    }
+    await browser.close();
+  } catch (error) {
+    console.log(
+      `Encountered error at ${new Date()} running check: ${error.message}`
+    );
   }
-  await browser.close();
 };
-
--- src/bestbuyps5disc.js
+++ src/bestbuyps5disc.js
@@ -4,36 +4,43 @@ const BestBuyPS5DiscConfig = require('./site_configs').bestbuyps5disc;
 const iPhone = devices['iPhone 11 Pro'];
 
 module.exports = async function () {
-  const browser = await chromium.launch({ headless: true });
-  const context = await browser.newContext({
-    ...iPhone,
-    javaScriptEnabled: false
-  });
-  const page = await context.newPage();
-  await page.goto(BestBuyPS5DiscConfig.url);
-  await page.waitForSelector(BestBuyPS5DiscConfig.selectors.stockAvailabilityContainer);
-  const currentAvailabilityStatus = await page.$eval(
-    BestBuyPS5DiscConfig.selectors.stockAvailabilityContainer,
-    (node) => {
-      return node.innerText;
-    }
-  );
-  console.log(
-    `${
-      BestBuyPS5DiscConfig.itemName
-    } status as of ${new Date()} —— ${currentAvailabilityStatus}`
-  );
-  if (
-    BestBuyPS5DiscConfig.hasSentMessage === false &&
-    currentAvailabilityStatus !== BestBuyPS5DiscConfig.outOfStockString
-  ) {
-    console.log('Would have sent a message');
-    await SendSlackMessage(
-      BestBuyPS5DiscConfig.changedMessage(),
-      BestBuyPS5DiscConfig.channelName
+  try {
+    const browser = await chromium.launch({ headless: true });
+    const context = await browser.newContext({
+      ...iPhone,
+      javaScriptEnabled: false,
+    });
+    const page = await context.newPage();
+    await page.goto(BestBuyPS5DiscConfig.url);
+    await page.waitForSelector(
+      BestBuyPS5DiscConfig.selectors.stockAvailabilityContainer
     );
-    BestBuyPS5DiscConfig.hasSentMessage = true;
+    const currentAvailabilityStatus = await page.$eval(
+      BestBuyPS5DiscConfig.selectors.stockAvailabilityContainer,
+      (node) => {
+        return node.innerText;
+      }
+    );
+    console.log(
+      `${
+        BestBuyPS5DiscConfig.itemName
+      } status as of ${new Date()} —— ${currentAvailabilityStatus}`
+    );
+    if (
+      BestBuyPS5DiscConfig.hasSentMessage === false &&
+      currentAvailabilityStatus !== BestBuyPS5DiscConfig.outOfStockString
+    ) {
+      console.log('Would have sent a message');
+      await SendSlackMessage(
+        BestBuyPS5DiscConfig.changedMessage(),
+        BestBuyPS5DiscConfig.channelName
+      );
+      BestBuyPS5DiscConfig.hasSentMessage = true;
+    }
+    await browser.close();
+  } catch (error) {
+    console.log(
+      `Encountered error at ${new Date()} running check: ${error.message}`
+    );
   }
-  await browser.close();
 };
-
